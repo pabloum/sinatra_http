8f3b41fc9e4a7d1cef2ae621942490c688065caa
Pablo <pablo.um00@gmail.com>
2018-07-31 15:53:45 -0500

    reto2 mejorado

--- solution.rb
+++ solution.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 
 get '/makers/:nombre' do
-
+    nombre = params[:nombre]
+    nombre[0] = nombre[0].upcase
     <<-HTML
-      <h1>Hola #{params[:nombre]}!</h1>
+      <h1>Hola #{nombre}!</h1>
     HTML
 end

769ac76c7f2c61b4862f501115e7e0e18703eaaa
Pablo <pablo.um00@gmail.com>
2018-07-31 15:46:07 -0500

    reto2

---
+++ reto1/solution.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+get '/' do
+
+  unless params[:nombre] && params[:nombre]!=''
+    <<-HTML
+        <h1>Hola desconocido!</h1>
+    HTML
+  else
+    <<-HTML
+      <h1>Hola #{params[:nombre]}!</h1>
+    HTML
+  end
+end
--- solution.rb
+++ solution.rb
@@ -1,14 +1,8 @@
 require 'sinatra'
 
-get '/' do
+get '/makers/:nombre' do
 
-  unless params[:nombre] && params[:nombre]!=''
     <<-HTML
-        <h1>Hola desconocido!</h1>
+      <h1>Hola #{params[:nombre]!}</h1>
     HTML
-  else
-    <<-HTML
-      <h1>Hola #{params[:nombre]}!</h1>
-    HTML
-  end
 end

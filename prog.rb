b66d72bc94db010697a444b0577c79b1ee8dbdff
Pablo <pablo.um00@gmail.com>
2018-07-31 20:58:26 -0500

    clase de sinatra

--- prog.rb
+++ prog.rb
@@ -1,5 +1,22 @@
 require "sinatra"
 
 get '/' do
-  'Salut, monde'
+  @name = 'Pablo'
+  erb :index
+end
+
+
+get '/home' do
+  @saludo = "Welcome to my first app with sinatra"
+end
+
+get '/contact' do
+  @telefono = 2381513
+  @email = "pablo@pablo.com"
+  erb :index
+end
+
+get '/products' do
+  @productos = [1,2,3,4,5]
+  erb :index
 end

021d7ec99dd7bd3c55dddf2bbfd6b86cc368ea4b
Pablo <pablo.um00@gmail.com>
2018-08-06 16:25:11 -0500

    proyecto plataforma sinatra

---
+++ proyecto_plataforma/app.rb
@@ -0,0 +1,35 @@
+require "sinatra"
+require "make_todo"
+require "httparty"
+require "pry-nav"
+
+
+get '/' do
+  @tareas = Tarea.all
+  erb :index
+end
+
+post '/create' do
+  Tarea.create(params[:titulo])
+  redirect '/'
+end
+
+post '/complete' do
+  Tarea.update(params[:id])
+  redirect '/'
+end
+
+post '/find' do
+  @busqueda = Tarea.find(params[:id])
+  erb :busqueda
+  # redirect '/'
+end
+
+post '/delete' do
+  Tarea.destroy(params[:id].to_i)
+  redirect '/'
+end
+
+delete '/' do
+
+end
